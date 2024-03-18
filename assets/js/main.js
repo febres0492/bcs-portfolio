45480c54da6d7ae23ab8fc8821e17744d317c956
Carlos Febres <febres0492@gmail.com>
2024-03-18 19:46:53 -0400

    updating image viewer logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,9 @@ function $ (str){
     if (str[0] ==='#'){ return document.querySelector(str) }
     else { return document.querySelectorAll(str) }
 }
+function capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1)
+}
 
 $('#nav-menu-btn').addEventListener('click', ()=>{
     $('nav ul')[0].classList.toggle('showMenu')
@@ -10,7 +13,17 @@ $('#nav-menu-btn').addEventListener('click', ()=>{
 
 // displaying project image
 $('#project-box').addEventListener('click',(ev)=>{
+
+    $('#image-viewer').classList.add('show-image-viewer')
+
+
+    $('#image-viewer-title').innerText = capitalize(ev.target.name)
+
     console.log(ev.target)
+})
+
+$('#close-btn').addEventListener('click',()=>{
+    $('#image-viewer').classList.remove('show-image-viewer')
 })
 
 // managing contact form event
