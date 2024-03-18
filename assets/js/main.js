cdd5e25e65d64b22671b8f95af986ffc4a8037ae
Carlos Febres <febres0492@gmail.com>
2024-03-18 18:44:15 -0400

    adding image viewer div

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,10 @@ $('#nav-menu-btn').addEventListener('click', ()=>{
     $('nav ul')[0].classList.toggle('showMenu')
 })
 
+// displaying project image
 $('#project-box').addEventListener('click',(ev)=>{
     console.log(ev.target)
 })
+
+// managing contact form event
+$('#contactForm').addEventListener('submit', ev =>{ev.preventDefault()})
