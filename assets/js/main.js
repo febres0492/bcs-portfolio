79932c37697e24535f2794b32be172f0419a8225
Carlos Febres <febres0492@gmail.com>
2024-03-24 18:00:59 -0400

    fixing contact section email icon placement

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,6 @@ S('#nav-menu-btn').addEventListener('click', ()=>{
     S('nav ul')[0].classList.toggle('showMenu')
 })
 
-// managing contact form event
-S('#contactForm').addEventListener('submit', ev =>{ev.preventDefault()})
-
 function S (str){
     if (str[0] ==='#' && !str.includes(' ')){ return document.querySelector(str) }
     else { return document.querySelectorAll(str) }
